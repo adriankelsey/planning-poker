7114e4e6592d2646771020369af6807126e31000
Adrian Kelsey <adriankelsey6@gmail.com>
2022-06-25 10:31:08 +0100

    created table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,23 @@ import { CardsComponent } from './cards/cards.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsComponent,
+    DataTableComponent,
   ],
   imports: [
     BrowserModule,
     NoopAnimationsModule,
-    MatCardModule,  
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
