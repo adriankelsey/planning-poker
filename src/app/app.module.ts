d6f88ffaabe201b8772f6787497bbffc0c73f064
Adrian Kelsey <adriankelsey6@gmail.com>
2022-07-10 15:01:15 +0100

    created table with 8 player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
-import { DataTableComponent } from './data-table/data-table.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { PlayerComponent } from './player/player.component';
+import { TableComponent } from './table/table.component';
 @NgModule({
   declarations: [
     AppComponent,
     CardsComponent,
-    DataTableComponent,
+    PlayerComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
