750af757ddbce12da940a0066346680453b3b268
Adrian Kelsey <adriankelsey@Adrians-MacBook-Pro.local>
2022-08-21 08:18:21 +0100

    functionality for highlighting cards

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -6,6 +6,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent {
+  cards: string[] = []
+  clickedCard: string[] = []
   constructor() {}
   ngOnInit(): void {
   }
@@ -16,12 +18,36 @@ export class CardsComponent {
 
     const card = document.getElementById(elementId)
 
-    // TODO: Logic for if card is clicked stay highlighted with color & if another card is clicked undo color
-    if (card != null)  {
-      if(card.style.backgroundColor == 'rgb(117, 82, 117)') {
-        card.style.backgroundColor = 'white'
-      }
-      card.style.backgroundColor = 'rgb(117, 82, 117)'
+    if (card != null) {
+      card.style.background = 'rgb(117, 82, 117)'
+      this.clickedCard.push(card.id)
+      if(this.clickedCard.length > 1) this.clickedCard.shift()
     }
+    
+    if (card?.id != undefined) this.cards.push(card?.id)
+
+    for(let i = 0; i < this.cards.length; i++) {
+      if(i > 0) {
+        const previousCard = document.getElementById(this.cards[i - 1])
+        if (previousCard != null) previousCard.style.background = 'white'
+        this.cards.shift()
+      }
+    }
+  }
+
+  mouseOver(event: Event) {
+    const elementId = (event.target as Element).id;
+    const card = document.getElementById(elementId)
+    if (card != null) card.style.background = 'rgb(117, 82, 117)'
+  }
+
+  mouseOut(event: Event) {
+    const elementId = (event.target as Element).id;
+    const card = document.getElementById(elementId)
+    const clickedCard = document.getElementById(this.clickedCard[0])
+
+    if (card?.id != clickedCard?.id && clickedCard != null) {
+      if(card != null) card.style.background = 'white'
+    } 
   }
 }
