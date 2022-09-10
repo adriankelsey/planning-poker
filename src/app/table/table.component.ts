fddf1cc505e27b4693c18148bb7f84507e787ffe
Adrian Kelsey <adriankelsey@Adrians-MacBook-Pro.local>
2022-09-10 09:21:17 +0100

    refactor of voting system

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -8,47 +8,72 @@ import { PlayerCard } from './models/card.model';
 })
 export class TableComponent implements OnInit {
 
-  show = false
-
   playerCard: PlayerCard[] = []
-
-  votingPhase = {
-    phase: '',
-    banner: '',
-    enabled: false,
-  }
 
   card: PlayerCard = {
     score: '',
     visible: false
   }
 
+  votingPhases = {
+    banner: '',
+    cardVisible: false,
+    startVisible: true,
+    finishVisible: false,
+    resetVisible: false,
+    rescoreVisible: false
+  }
+
   // playerScore = ''
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.votingPhase.phase = 'Start Vote'
-  }
+  ngOnInit(): void {}
 
   receivedMessage($event: any){
     this.card.score = $event
     // this.playerScore = $event
   }
 
-  votePhase() {
-    if(this.votingPhase.phase == 'Start Vote') {
-      this.votingPhase.phase = 'Finish Vote'
-      this.votingPhase.banner = 'Voting has started'
-      this.votingPhase.enabled = true
-    } else if (this.votingPhase.phase == 'Finish Vote') {
-      this.card.visible = true
-      this.votingPhase.phase = 'Start Vote'
-      this.votingPhase.enabled = false
-      this.votingPhase.banner = ''
-    }
-  } 
+  // votePhase() {
+  //   if(this.votingPhase.phase == 'Start Vote') {
+  //     this.votingPhase.phase = 'Finish Vote'
+  //     this.votingPhase.banner = 'Voting has started'
+  //     this.votingPhase.enabled = true
+  //   } else if (this.votingPhase.phase == 'Finish Vote') {
+  //     this.card.visible = true
+  //     this.votingPhase.phase = 'Start Vote'
+  //     this.votingPhase.enabled = false
+  //     this.votingPhase.banner = ''
+  //   }
+  // } 
 
+  startVote() {
+    this.votingPhases.banner = 'VOTING HAS STARTED'
+    this.votingPhases.finishVisible = true
+    this.votingPhases.startVisible = false
+  }
+
+  finishVote() {
+    this.votingPhases.banner = 'VOTING HAS FINISHED'
+    this.votingPhases.finishVisible = false
+    this.votingPhases.resetVisible = true
+    this.votingPhases.rescoreVisible = true
+  }
+
+  resetVote() {
+    this.votingPhases.banner = ''
+    this.votingPhases.resetVisible = false
+    this.votingPhases.rescoreVisible = false
+    this.votingPhases.startVisible = true
+  }  
+
+  rescoreVote() {
+    this.votingPhases.banner = "RESCORING"
+    this.votingPhases.resetVisible = false
+    this.votingPhases.rescoreVisible = false
+    this.votingPhases.finishVisible = true
+  }
   
 
 }
