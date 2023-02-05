01541d0731dc19766c4367fdb133ec53a06104e5
Adrian Kelsey <adriankelsey@Adrians-MacBook-Pro.local>
2023-02-05 10:58:21 +0000

    adding animation for average scores

--- src/app/average-scores-component/average-scores.component.ts
+++ src/app/average-scores-component/average-scores.component.ts
@@ -25,6 +25,7 @@ export type user = {
 })
 export class AverageScoresComponent implements AfterViewInit {
   average: BehaviorSubject<number> = new BehaviorSubject(0);
+  averageProgress: BehaviorSubject<number> = new BehaviorSubject(0);
   sum: BehaviorSubject<number> = new BehaviorSubject(0);
   users: BehaviorSubject<any[]> = new BehaviorSubject([] as any[]);
   isVisible: BehaviorSubject<boolean> = new BehaviorSubject(false);
@@ -56,31 +57,32 @@ export class AverageScoresComponent implements AfterViewInit {
         let sum = scores.reduce((a, b) => parseInt(a) + parseInt(b));
         let average = sum / scores.length;
 
-        this.average.next(Math.round(average));
+        this.average.next(Math.round((average + Number.EPSILON) * 100) / 100);
       }
     });
 
     this.isVisible.subscribe((value) => {
-      console.log(value);
       if (this.component) {
         let progressValue = 0;
-
-        let interval = setInterval(() => {
+        let averageProgress = 0;
+        let averageInterval = this.average.getValue() / 360;
+        let progressInterval = setInterval(() => {
           progressValue++;
+          averageProgress = averageProgress + averageInterval;
+          this.averageProgress.next(
+            Math.round((averageProgress + Number.EPSILON) * 100) / 100
+          );
           this.renderer.setStyle(
             this.component?.nativeElement,
             'background',
             `conic-gradient(#ffd63f  ${progressValue}deg, rgba(187, 187, 187, 0.333) 0deg)`
           );
-          if (progressValue === 360) clearInterval(interval);
-        }, 10);
+          if (progressValue === 360) clearInterval(progressInterval);
+        }, 1);
       }
     });
   }
-  ngAfterViewInit(): void {
-    console.log('hello');
-    console.log(this.component?.nativeElement);
-  }
+  ngAfterViewInit(): void {}
 
   ngOnInit(): void {
     console.log(this.component);
